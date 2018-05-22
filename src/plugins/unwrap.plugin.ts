3aa63b8bc754cb14ca60869ea9ea929974fa073d
Dereck Bridie <d.j.bridie@student.tudelft.nl>
2018-05-22 14:03:16 +0900

    Add map expression over array type

--- src/plugins/unwrap.plugin.ts
+++ src/plugins/unwrap.plugin.ts
@@ -1,7 +1,31 @@
 import * as babel from "babel-core";
-import {isIdentifier, MemberExpression} from "babel-types";
+import {
+    arrowFunctionExpression,
+    callExpression,
+    ExpressionStatement,
+    identifier,
+    isCallExpression,
+    isIdentifier,
+    isMemberExpression,
+    memberExpression,
+    MemberExpression,
+    Node,
+} from "babel-types";
 import {isJQueryWrappedElement, unWrapjQueryElement} from "../util/jquery-heuristics";
 import {NodePath} from "babel-traverse";
+
+function innerIsArray(node: Node) {
+    if (isCallExpression(node)) {
+        const arrayReturnCalls = ["querySelectorAll", "getElementsByClassName"];
+        if (isMemberExpression(node.callee)) {
+            const callee = node.callee;
+            if (!isIdentifier(callee.property)) return false;
+            const id = callee.property.name;
+            if (arrayReturnCalls.some(call => call === id)) return true;
+        }
+    }
+    return false;
+}
 
 export const UnwrapPlugin = () => ({
     visitor: {
@@ -15,7 +39,22 @@ export const UnwrapPlugin = () => ({
                 if (node.property.name !== "0") return;
                 if (!isJQueryWrappedElement(node.object)) return;
                 const arg = unWrapjQueryElement(node.object);
-                path.replaceWith(arg);
+                if (innerIsArray(arg)) {
+                    const map = memberExpression(arg, identifier("map"));
+                    const elementId = path.scope.generateUidIdentifier("element");
+
+                    path.replaceWith(elementId);
+                    const parentStatement = path.getStatementParent() as NodePath<ExpressionStatement>;
+                    const parentExpr = parentStatement.node.expression;
+
+                    const lambda = arrowFunctionExpression([elementId], parentExpr);
+                    const mapCall = callExpression(map, [lambda]);
+
+                    parentStatement.replaceWith(mapCall);
+
+                } else {
+                    path.replaceWith(arg);
+                }
             },
         },
     } as babel.Visitor<{}>,
