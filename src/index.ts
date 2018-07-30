6c00680ed90b7f534315340c1540cb9e684edc81
Dereck Bridie <d.j.bridie@student.tudelft.nl>
2018-07-30 17:31:37 +0900

    Remove cross-env. Remove experimental API usage.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
 import * as babel from "babel-core";
-import * as fs from "fs";
-import {promisify} from "util";
 import {jQueryExpressionPlugin} from "./plugins/jquery-expression.plugin";
 import {plugins} from "./all-plugins";
 
 const cli = require("command-line-parser");
 
-(async () => {
-    const args: any = cli();
-    const file = (args._args && args._args[0]) || undefined;
-    const withInlineSourceMap = args.withInlineSourceMap;
-    if (!file) {
-        console.error("Please specify a file to convert as the first argument to this script.");
-        process.exit(1);
-        return;
-    }
-    const fileContents = await promisify(fs.readFile)(file, "utf8");
-    const transformed = babel.transform(fileContents, {
+const args: any = cli();
+const file = (args._args && args._args[0]) || undefined;
+const withInlineSourceMap = args.withInlineSourceMap;
+if (file) {
+    const transformed = babel.transformFileSync(file, {
         plugins: [jQueryExpressionPlugin(plugins)],
         sourceMaps: withInlineSourceMap ? "inline" : false,
         sourceFileName: file,
         ast: false,
     });
     console.log(transformed.code);
-})();
+} else {
+    console.error("Please specify a file to convert as the first argument to this script.");
+    process.exit(1);
+}
